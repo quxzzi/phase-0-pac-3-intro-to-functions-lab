5eda4b255f8569673ecaa80e66dba183dd8ae709
Kevin Dean <kdean1011@gmail.com>
2023-04-07 16:38:38 -0400

    7 passed test!

--- test/indexTest.js
+++ test/indexTest.js
@@ -52,3 +52,36 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
     );
   });
 })
+function shout(string) {
+  return string;
+}
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string;
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string){
+  uppercase=string.toUpperCase();
+  string=uppercase;
+  console.log (uppercase);
+}
+function logWhisper(string){
+  lowercase=string.toLowerCase();
+  string=lowercase;
+  console.log (lowercase);
+}
+function sayHiToHeadphonedRoommate(string){
+  if (string.toLowerCase() === string){
+    return "I can't hear you!";
+  }
+  if (string.toUpperCase() ===string){
+    return "YES INDEED!";
+  }
+  if (string === "Let's have dinner together!"){
+    return 'I would love to!';
+  }
+}
